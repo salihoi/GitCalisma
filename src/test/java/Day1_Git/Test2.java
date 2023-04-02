11ee631449df46a3021f758a2b769040b17b2fdc
AlexWedon <alex_wedon@hotmail.com>
2023-04-02 21:47:42 +0300

    commit

--- src/test/java/Day1_Git/Test2.java
+++ src/test/java/Day1_Git/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
     public static void main(String[] args) {
 
         System.out.println("İkinci Git Class'ı");
+        System.out.println("salih abi, salih abi, salih abi");
 
     }
 }
